105505cc53ed4b2c1804597dfd61221fbd79e195
Luke Cheng <Lukeecheng@gmail.com>
2024-10-04 22:53:05 -0700

    ep 2. jsx, components, props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
 
 function App() {
+  const name = <h1>Luke</h1>;
+  const age = <h2>25</h2>
+  const email = <h2>luke@gmail.com</h2>
+  const combine = (<div>
+                    {name}
+                    {age}
+                    {email}
+                  </div>);
+
+  
+  const GetNameComponent = () => {
+    return (
+    <div>
+      <h1>Luke component</h1>
+      <h1>21 component</h1>
+      <h1>email component</h1>
+    </div>
+    );
+  };
+
+  const PropsComponent = (props) => {
+    return (
+      <div>
+        <h1>{props.name}</h1>
+        <h1>{props.age}</h1>
+        <h1>{props.email}</h1>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      Hello Tilak
+    {combine}
+    <GetNameComponent />
+    <PropsComponent name="Julia" age={18} email="julia@gmail.com" />
+    <PropsComponent name="Denise" age={28} email="denise@gmail.com" />
     </div>
   );
+
+
+
+  
 }
+
 
 export default App;
