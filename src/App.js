f016cdefdd68a15d1c29f0b62cf771dce49322e8
Luke Cheng <Lukeecheng@gmail.com>
2024-10-05 16:25:56 -0700

    ep3. css, ternary operators, list & object, exporting $ importing components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import {Object} from './User'
 
 function App() {
-  const name = <h1>Luke</h1>;
-  const age = <h2>25</h2>
+  const name = <h1 className="name">Luke</h1>;
+  const age = 25
+  const isBlue = true
+  const button = true
   const email = <h2>luke@gmail.com</h2>
   const combine = (<div>
                     {name}
@@ -31,18 +34,42 @@ function App() {
     );
   };
 
+  // list / array
+  const names = ['name1', 'name2', 'name3', 'name4', 'name5'];
+
+  // list of objects
+  const objects = [
+    {name: 'name1', age: 21},
+    {name: 'name2', age: 11},
+    {name: 'name3', age: 31},
+  ]
+
+  
+
   return (
     <div className="App">
     {combine}
     <GetNameComponent />
     <PropsComponent name="Julia" age={18} email="julia@gmail.com" />
     <PropsComponent name="Denise" age={28} email="denise@gmail.com" />
+    {/* show h1 tag is age >= 18 */}
+    {/* turn colour to blue if blue = true */}
+    {age >= 18 ? <h1 style={{color: isBlue ? 'blue' : 'green'}} > Luke is over age</h1> : <h1>Luke is under age</h1>}
+    {/* show the button if button is true */}
+    {button ? <button> BUTTON IS TRUE</button> : null}
+    <h1>Printing out list of names below using .map()</h1> 
+    {names.map((value, key) => {
+       return <h3>{value}</h3>
+    })}
+
+    <h1>Printing out the list of objects below</h1>
+    {objects.map((value, key) => {
+      return <Object name={value.name} age={value.age}/>
+
+    })}
     </div>
   );
 
-
-
-  
 }
 
 
---
+++ src/User.js
@@ -0,0 +1,6 @@
+// this file stores all the components
+
+// component / prop for objects
+export const Object = (props) => {
+  return <div>{props.name}, {props.age}</div>
+}
