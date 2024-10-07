1dbe3ededc5b14c9436796ac28ef118924ad81d7
Luke Cheng <Lukeecheng@gmail.com>
2024-10-07 12:12:50 -0700

    ep 10. React query

--- src/App2.js
+++ src/App2.js
@@ -6,31 +6,39 @@ import { Menu } from './pages/Menu';
 import { Contact } from './pages/Contact';
 import { Navbar } from './Navbar';
 import { Profile } from './pages/Profile';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // createContext / useContext example
 export const AppContext = createContext();
 
-const App2 = () => {
+function App2() {
+  // example of React Query 
+  // queries (Read) or mutations(Create, Update, Delete)
+  // {defaultOptions: { queries: refetchOnWindowFocus: false  )} can be added in the QueryCline to prevent the page from reloading itself after switching tab and coming back
+  const client = new QueryClient();
+
   // const below is example for useContext hookg
   const [username, setUsername] = useState("LukeTech")
 
   return (
     <div className='App'>
-      {/* createContext provided that will provide data to the elements included */}
-      <AppContext.Provider value={{username, setUsername}}>
-      {/* router dom example */}
-        <Router>
-          <Navbar />
-            <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route path='/profile' element={<Profile />} />
-              <Route path='/menu' element={<Menu />}/>
-              <Route path='/contact' element={<Contact />}/>
-              {/* error page */}
-              <Route path='*' element={<h1>Error Page</h1>} />
-            </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        {/* createContext provided that will provide data to the elements included */}
+        <AppContext.Provider value={{username, setUsername}}>
+        {/* router dom example */}
+          <Router>
+            <Navbar />
+              <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/menu' element={<Menu />}/>
+                <Route path='/contact' element={<Contact />}/>
+                {/* error page */}
+                <Route path='*' element={<h1>Error Page</h1>} />
+              </Routes>
+          </Router>
+        </AppContext.Provider> 
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
 import { useContext } from "react";
 import { AppContext } from "../App2";
+import { useQuery } from '@tanstack/react-query'
+import Axios from "axios"
 
 export const Home = (props) => {
+
+  // useQuery example to utilize the data fetched using axios
+  // :catData can be added after data to be more obvios
+  const { data, isLoading, isError, refetch } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () => Axios.get("https://catfact.ninja/fact").then((res) => res.data),
+  });
+
   // useContext retrieves the data from the provider
   const {username} = useContext(AppContext); 
-  return <h1>THIS IS THE HOME PAGE and the user is :{username}</h1>;
+
+  // if the website is leading
+  if (isLoading) {
+    return <h1>Loading</h1>
+  }
+
+  if (isError) {
+    return <h1>Error encountered!</h1>
+  }
+
+  
+  return (
+    <div>
+    <h1>THIS IS THE HOME PAGE and the user is :{username}</h1>
+    <h1>This is the result of useQuery: {data.fact}</h1>
+    <button onClick={refetch}>Refetch Data</button>
+    </div>
+  );
 
 }
